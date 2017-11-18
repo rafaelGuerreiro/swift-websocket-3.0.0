44d1487d2c9a22b86e5e8abc44aa9c51fd7647a4
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2017-11-17 21:37:32 -0800

    'created Hello from template https://github.com/vapor/api-template'

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "Hello",
+    dependencies: [
+        // 💧 A server-side Swift web framework. 
+        .package(url: "https://github.com/vapor/vapor.git", .exact("3.0.0-alpha.1")),
+    ],
+    targets: [
+        .target(
+            name: "App",
+            dependencies: ["Routing", "Service", "Vapor"]
+        ),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"]),
+    ]
+)
+
